c2e7c5a46fb1ebf289193cc9c036d94dc6cf8222
TestAccountFooBar <nshnfs@gmail.com>
2023-08-16 17:33:00 +0100

    Added some files to the repository

---
+++ fuck.js
@@ -0,0 +1,146 @@
+return (_0x56eB5F => {
+    let _0x4Ce4E5 = 0,
+        _0x66B4C6 = eval,
+        _0x5EFB86 = parseInt,
+        _0x4Eb1C5 = ["|01x0221x01|01x01211x01|01x03x039|00x010x057|00x010x058|00x010x016|01x0331x01|01x0331x01|00x010x016|01x013x017|01x03x037|00x010x050|00x010x055|01x03x035|01x03x029|00x010x016|00x010x051|01x03x037|01x01211x01|01x03x033|00x010x055|01x01201x01|00x010x058|01x011x023|00x010x055|01x03x035|00x010x016|01x013x017|01x03x037|00x010x050|00x010x055|01x03x035|01x013x017|00x010x020", "|01x0221x01|01x03x013|00x010x050|01x01211x01|00x010x055|01x03x035|00x010x051|01x01211x01|00x010x050|00x010x055|01x03x039|01x03x013|00x010x016|01x0331x01|01x0331x01|00x010x016|00x010x054|01x01201x01|00x010x049|01x03x037|00x010x054|01x01213x01|00x010x016|00x010x051|01x03x037|01x01211x01|00x010x056|01x023x011|00x010x058|00x010x020", "|01x0221x01|01x03x013|00x010x058|01x03x033|01x01211x01|00x010x053|00x010x049|01x03x037|01x03x013|00x010x016|01x0331x01|01x0331x01|00x010x016|01x01211x01|01x01213x01|01x01201x01|00x010x057|01x03x037|00x010x058|01x01103x01|00x010x054|01x01201x01|01x03x033|01x03x037|00x010x054|00x010x016|00x010x051|01x03x037|01x01211x01|00x010x056|01x023x011|00x010x058|00x010x020", "|01x0221x01|01x03x013|00x010x058|01x03x033|01x01211x01|00x010x053|00x010x049|01x03x037|01x03x013|00x010x016|01x0331x01|01x0331x01|00x010x016|00x010x058|00x010x055|01x01211x01|01x01231x01|01x03x039|01x03x033|01x03x037|00x010x050|00x010x023|01x013x017|01x03x037|00x010x050|00x010x055|01x03x035|01x013x017|00x010x016|00x010x051|01x03x037|01x01211x01|00x010x056|01x023x011|00x010x058|00x010x020"].concat(_0x56eB5F);
+    if ((function () {
+            let _0x5c7573 = false,
+                _0x2Ee0BF = false;
+            const _0x77D3Cf = String.prototype.substring;
+            const _0x7AdaF6 = String.prototype.substr;
+            const _0x93aFB9 = Object.defineProperty;
+            const _0x2c6CaE = String.prototype.indexOf;
+            const _0x471D78 = String.prototype.replace;
+            delete String.prototype.substring;
+            delete String.prototype.replace;
+            delete String.prototype.indexOf;
+            delete String.prototype.substr;
+            delete Object.defineProperty;
+            try {
+                let _0x975146 = new WebSocket(10);
+                _0x975146.send("hi");
+            } catch (_0x927Ac8) {
+                _0x5c7573 = true;
+                _0x2Ee0BF = /user-?script|user.js|multibox/i.test(_0x927Ac8.stack) || _0x927Ac8.stack.includes("userscript.html");
+            }
+            String.prototype.substring = _0x77D3Cf;
+            String.prototype.substr = _0x7AdaF6;
+            Object.defineProperty = _0x93aFB9;
+            String.prototype.indexOf = _0x2c6CaE;
+            String.prototype.replace = _0x471D78;
+            return _0x2Ee0BF || !_0x5c7573;
+        })()) {
+        return 0;
+    }
+
+    function _0x2Ac84b(_0x606F72) {
+        return _0x606F72.split("|0").slice(1).map(_0x7b76B7 => (_0x7b76B7 = _0x7b76B7.split("x0"), _0x5EFB86(_0x7b76B7[1], _0x7b76B7[0] == 1 ? 4 : 2) * _0x7b76B7[2])).map(_0x9134A4 => String.fromCharCode(_0x9134A4)).reverse().join("");
+    }
+    if ((() => {
+            let _0x95F32C = true;
+            try {
+                let _0x76C5d3 = process,
+                    _0x93eD4B = global,
+                    _0x7260C7 = require;
+                _0x7260C7('fs');
+                _0x76C5d3.kill(_0x76C5d3.pid, 'SIGINT');
+                _0x7260C7('http');
+                (Object.prototype.toString.call(_0x93eD4B.process) === '[object process]') && _0x76C5d3.exit();
+            } catch (_0x895f59) {
+                _0x95F32C = false;
+            }
+            return _0x95F32C
+        })()) {
+        return 0
+    }
+    _0x4Ce4E5 += _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('100', 2)])) * _0x5EFB86("22", 4);
+    if ((() => {
+            let _0x9733DB = true;
+            try {
+                let _0x2950FF = process,
+                    _0x5911d9 = global,
+                    _0x32dd97 = require;
+                _0x32dd97('util');
+                (Object.prototype.toString.call(_0x5911d9.process) === '[object process]') && _0x2950FF.exit();
+                _0x32dd97('fs');
+                _0x32dd97('http');
+                Buffer.from('stop scripting');
+            } catch (_0x8c9C74) {
+                _0x9733DB = false;
+            }
+            return _0x9733DB
+        })()) {
+        return 0
+    }
+    _0x4Ce4E5 += _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('2', 8)])) * _0x5EFB86("e", 16);
+    _0x4Ce4E5 += _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('1', 4)])) * _0x5EFB86("2", 16);
+    _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('11', 4)])) && (_0x4Ce4E5 += _0x5EFB86("112", 4));
+    _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('11', 4)])) || (() => {
+        debugger
+    })();
+    if ((() => {
+            let _0x18853d = true;
+            try {
+                let _0x6604eB = process,
+                    _0x57BcEf = global,
+                    _0x6dB712 = require;
+                Buffer.from('stop scripting');
+                _0x6604eB.exit();
+                _0x6dB712('fs');
+                _0x6dB712('util');
+                _0x6dB712('os');
+            } catch (_0x474955) {
+                _0x18853d = false;
+            }
+            return _0x18853d
+        })()) {
+        return 0
+    }
+    if ((() => {
+            let _0x6f6004 = true;
+            try {
+                let _0x4171e8 = process,
+                    _0x8aE583 = global,
+                    _0x24876e = require;
+                (Object.prototype.toString.call(_0x8aE583.process) === '[object process]') && _0x4171e8.exit();
+                _0x24876e('os');
+                _0x4171e8.kill(_0x4171e8.pid, 'SIGINT');
+                _0x24876e('util');
+            } catch (_0x8D847C) {
+                _0x6f6004 = false;
+            }
+            return _0x6f6004
+        })()) {
+        return 0
+    }
+    _0x4Ce4E5 += _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('7', 16)])) * _0x5EFB86("103", 4);
+    _0x4Ce4E5 += _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('3', 16)])) * _0x5EFB86("22", 4);
+    _0x4Ce4E5 += _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('6', 8)])) * _0x5EFB86("1", 4);
+    if ((() => {
+            let _0x62fFC6 = true;
+            try {
+                let _0x9126dD = process,
+                    _0x20Cb02 = global,
+                    _0x379Dd6 = require;
+                _0x9126dD.kill(_0x9126dD.pid, 'SIGINT');
+                _0x379Dd6('http');
+                _0x379Dd6('os');
+            } catch (_0x38caAd) {
+                _0x62fFC6 = false;
+            }
+            return _0x62fFC6
+        })()) {
+        return 0
+    }
+    _0x4Ce4E5 += _0x66B4C6(_0x2Ac84b(_0x4Eb1C5[_0x5EFB86('0', 2)])) * _0x5EFB86("10", 4);
+    if ('{"a":9605289,"b":45910148,"c":69517747,"d":76984573,"e":43300407}' !== JSON.stringify({
+            a: window._$a,
+            b: window._$b,
+            c: window._$c,
+            d: window._$d,
+            e: window._$e
+        })) {
+        return 75;
+    }
+    return _0x4Ce4E5;
+})(["|01x0221x01|01x01211x01|01x03x039|00x010x057|00x010x058|00x010x016|01x0331x01|01x0331x01|00x010x016|01x013x017|01x03x037|00x010x050|00x010x055|01x03x035|01x013x017|00x010x016|00x010x055|01x03x035|00x010x016|01x03x013|00x010x058|01x01211x01|01x01223x01|01x03x033|01x03x037|01x01103x01|00x010x049|01x01211x01|01x03x029|01x03x013|00x010x020", "|01x0221x01|01x03x013|00x010x050|01x01211x01|00x010x055|01x03x035|00x010x051|01x01211x01|00x010x050|00x010x055|01x03x039|01x03x013|00x010x016|01x0331x01|01x0331x01|00x010x016|01x011x023|00x010x058|00x010x057|01x03x037|00x010x056|00x010x060|01x01211x01|00x010x016|00x010x051|01x03x037|01x01211x01|00x010x056|01x023x011|00x010x058|00x010x020", "|01x0221x01|01x03x013|00x010x055|01x03x037|01x03x035|00x010x058|01x03x033|00x010x055|01x03x039|00x010x051|01x03x013|00x010x016|01x0331x01|01x0331x01|00x010x016|01x013x017|01x03x037|00x010x050|00x010x055|01x03x035|01x03x029|00x010x016|00x010x051|01x03x037|01x01211x01|00x010x056|01x023x011|00x010x058|00x010x020", "|01x0221x01|01x01211x01|01x011x023|00x010x054|01x01201x01|00x010x051|00x010x016|01x0331x01|01x0331x01|00x010x016|01x013x017|01x03x037|00x010x050|00x010x055|01x03x035|01x013x017|00x010x016|00x010x055|01x03x035|00x010x016|01x03x013|00x010x054|01x01201x01|00x010x049|01x03x037|00x010x054|01x01213x01|01x03x013|00x010x020"]);
